3a28cad59a0b04410c3c58d6cce1d7f31fd20ef5
ramihesham <148961249+ramihesham@users.noreply.github.com>
2023-11-25 01:48:07 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+from tkinter import *
 class TypeSpeedGUI:
-    def __init__(self):
-        self.window= tk.Tk()
-        self.window.iconbitmap('icon.ico')
-        self.window.geometry('1000x600+250+125')
-        self.window.resizable(0, 0)
-        self.window.title("speed typing application  (expert level) ")
-        self.window.config(bg='cyan3')
-        self.window.mainloop()
+    def __init__(gui3):
+        gui3.window= tk.Tk()
+        gui3.window.iconbitmap('icon.ico')
+        gui3.window.geometry('1000x600+250+125')
+        gui3.window.resizable(0, 0)
+        gui3.window.title("speed typing application  (expert level) ")
+        gui3.window.config(bg='cyan3')
+        timepic = PhotoImage(file='time.png')
+        piclabel = Label(gui3.window, image=timepic, bg='cyan3')
+        piclabel.place(x=425, y=150)
+        gui3.window.mainloop()
 TypeSpeedGUI()
